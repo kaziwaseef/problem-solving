ed90345e452fd2ff489a60ec9c112724733d4459
Kazi Waseef <kaziwaseef@gmail.com>
2023-03-25 02:10:00 +0600

    feat: reversed link list

--- blind75/week2/reversedLinkList.go
+++ blind75/week2/reversedLinkList.go
@@ -1,1 +1,22 @@
 // Paste in leetcode: https://leetcode.com/problems/reverse-linked-list/
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	var reversed *ListNode
+	var next *ListNode
+	for head != nil {
+		next = head.Next
+		head.Next = reversed
+		reversed = head
+		head = next
+	}
+	return reversed
+}
